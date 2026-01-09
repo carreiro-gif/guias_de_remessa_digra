e7f9182ffe375c2d4031aad7ef418499baf2e1d1
carreiro-gif <espacocarreiro@gmail.com>
2026-01-09 14:54:31 -0300

    Remove Firebase app initialization and configremove firebase duplicate initialization
    Removed Firebase initialization code and configuration.

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,14 +1,1 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDbntaHfsUNK-dHUtS6aYp_CNi2b4jB2N8",
-  authDomain: "guia-de-remessa-digra.firebaseapp.com",
-  projectId: "guia-de-remessa-digra",
-  storageBucket: "guia-de-remessa-digra.firebasestorage.app",
-  messagingSenderId: "694403541232",
-  appId: "1:694403541232:web:258e668cebff2dee5b3a35",
-};
-
-export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
