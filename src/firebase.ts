4f0ec3f214b67efb7694c9a7382f869762a65eb8
carreiro-gif <espacocarreiro@gmail.com>
2026-01-09 14:29:39 -0300

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,15 +1,13 @@
-// src/firebase.ts
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// ⚠️ NÃO ALTERE OS NOMES DAS VARIÁVEIS
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDbntaHfsUNK-dHUtS6aYp_CNi2b4jB2N8",
+  authDomain: "guia-de-remessa-digra.firebaseapp.com",
+  projectId: "guia-de-remessa-digra",
+  storageBucket: "guia-de-remessa-digra.firebasestorage.app",
+  messagingSenderId: "694403541232",
+  appId: "1:694403541232:web:258e668cebff2dee5b3a35",
 };
 
 export const app = initializeApp(firebaseConfig);
